b7037dc0f6b588e13359df5c4f8c256e086ca648
Julia Nicola <julia.nicola@hotmail.com>
2019-10-25 23:36:01 -0300

    inicio implementacao view

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.set('views', path.join(__dirname, 'view'));
+app.set('views', path.join(__dirname, 'src/view'));
 app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'src/static')));
 
 app.get('/', function(req, res){
     res.render('Principal');
